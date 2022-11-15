e61f024d437f04cd04cfc509645a2e1770c1207b
Mariusz <coder.mariusz@gmail.com>
2022-11-15 23:48:12 +0000

    add style to menu and add js function toggle open and close manu

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ import myjson from "./data.json" assert { type: "json" };
 
 function start() {
   console.log("hej!");
+  addListener();
+}
+
+function toggleMenu() {
+  const nav = document.querySelector("#nav");
+  nav.classList.toggle("open");
+}
+function addListener() {
+  const menu = document.querySelector("#menu");
+  const closeMenu = document.querySelector("#menu-close");
+  console.log(menu);
+  console.log(closeMenu);
+  menu.addEventListener("click", toggleMenu);
+  closeMenu.addEventListener("click", toggleMenu);
 }
 
 start();
