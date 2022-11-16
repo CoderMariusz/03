a7d521c9237e4bb4ac231eec03993ac855b9b4d5
Mariusz <coder.mariusz@gmail.com>
2022-11-16 00:17:38 +0000

    addeven listener and make toggle function to menu

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-import myjson from "./data.json" assert { type: "json" };
-
 function start() {
   console.log("hej!");
   addListener();
 }
 
-function toggleMenu() {
+function toggleMenuOpen() {
   const nav = document.querySelector("#nav");
   nav.classList.toggle("open");
 }
+
 function addListener() {
   const menu = document.querySelector("#menu");
   const closeMenu = document.querySelector("#menu-close");
-  console.log(menu);
-  console.log(closeMenu);
-  menu.addEventListener("click", toggleMenu);
-  closeMenu.addEventListener("click", toggleMenu);
+
+  menu.addEventListener("click", toggleMenuOpen);
+  closeMenu.addEventListener("click", toggleMenuOpen);
 }
 
 start();
